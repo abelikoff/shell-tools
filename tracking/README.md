45d30590722c9aa5fd4cfed64a8d62ac727c703e
Alexander L. Belikoff <abelikoff@google.com>
2015-06-02 16:15:17 -0400

    Time tracking tools

---
+++ tracking/README.md
@@ -0,0 +1,12 @@
+Time tracking tools
+===================
+
+Simple tools for time tracking and report generation.
+
+
+`time-track.el` is an Emacs script for time tracking. Use interactive
+function `time-track-add-entry` to add new entry (default tracking log
+is `~/.time-track.log`).
+
+
+`gen_weekly_report` generates report for a given week.
---
+++ tracking/time-track.el
@@ -0,0 +1,37 @@
+(defvar time-track-file (expand-file-name "~/.time-track.log"))
+(defvar time-track-categories
+  '("Clients"
+    "Coding"
+    "Cross-product"
+    "Meta"
+    "Misc"
+    "Other meetings"
+    "People management"
+    "Personal"
+    "Project management"
+    "Recruiting"
+    "Strategy/Planning"
+    "Team meta"))
+
+(defun time-track-add-entry (minutes category activity outcome)
+  "Track time using ad-hoc entries. Entries are saved in timer-track-file."
+
+  (interactive (list
+                (read-number "Minutes: ")
+                (read-string "Category: " nil 'time-track-categories)
+                (read-string "What did you do: ")
+                (read-string "Outcome: ")))
+  (let ((today (format-time-string "%m/%d/%Y")))
+    (save-excursion
+      (set-buffer (find-file-noselect time-track-file))
+      (widen)
+      (end-of-buffer)
+      ;; try to append new entry to the others with the same date
+      (search-backward-regexp (concat "^" today) nil t)
+      (if (not (string= (thing-at-point 'line) ""))
+          (progn
+            (move-end-of-line nil)
+            (insert "\n")))
+      (insert (format "%s | %d | %s | %s | %s"
+                      today minutes category activity outcome))
+      (save-buffer))))
